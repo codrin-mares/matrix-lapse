3591b359526ed2c33b2c20efea11751340c82df0
Codrin Mares <codrin.mares@gmail.com>
2024-07-12 21:35:20 +0300

    test6

--- src/components/Controls/Range/index.tsx
+++ src/components/Controls/Range/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 const Range = ({ value, onChange, min = 0, max }: Props): JSX.Element => {
-  console.log('LOG4');
+  console.log('LOG5');
   return (
     <div className="mb-8 flex-grow">
       <div className="flex justify-between pb-4">
