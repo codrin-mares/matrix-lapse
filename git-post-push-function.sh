6f1fde3db7a326f7d88f025f29a732be5eef3140
Codrin Mares <codrin.mares@gmail.com>
2024-07-15 12:29:49 +0300

    wip.

--- git-post-push-function.sh
+++ git-post-push-function.sh
@@ -1,11 +1,16 @@
 
-git() {
-  echo "In git function>>>>>>>>>>>>>>"
-  ROOT="$(/usr/bin/git rev-parse --show-toplevel)"
-  LOCATION="/.git/hooks/post-push"
-  if [ "$1" == "push" ] && [ -f "$ROOT$LOCATION" ]; then
-    /usr/bin/git $* && eval $ROOT$LOCATION
-  else
-    /usr/bin/git $*
-  fi
-}
+# git() {
+#   echo "In git function>>>>>>>>>>>>>>"
+#   ROOT="$(/usr/bin/git rev-parse --show-toplevel)"
+#   LOCATION="/.git/hooks/post-push"
+#   if [ "$1" == "push" ] && [ -f "$ROOT$LOCATION" ]; then
+#     /usr/bin/git $* && eval $ROOT$LOCATION
+#   else
+#     /usr/bin/git $*
+#   fi
+# }
+
+while IFS= read -r line
+do
+    echo "LINe: $line"
+done < /dev/stdout
